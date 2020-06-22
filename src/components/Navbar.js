10201be1186fe5c975fc5cf60326dd8777a651bf
Claudia Petty <claudialove32@gmail.com>
2020-06-22 14:20:43 -0400

    adding js functions to create style interactivity for ACTIVE links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,22 @@ import Navitem from './Navitem';
 //later the component will have sub-components (navitems)
 //each navitem sub-component has 2 props item (the display) and tolink (the route)
 class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'NavItemActive': ''
+        }
+    }
+    activeitem=(x)=> 
+    {
+        if(this.state.NavItemActive.length>0) {
+            document.getElementById(this.state.NavItemActive).classList.remove('active');
+        }
+        this.setState({'NavItemActive':x}, ()=> {
+            document.getElementById(this.state.NavItemActive).classList.add('active');
+        });
+    };
+
     render() {
         return (
             <nav>
--- src/components/Navitems.js
+++ src/components/Navitems.js
@@ -6,7 +6,7 @@ class Navitem extends Component {
     render() {
         return (
             <li>
-                <Link to ={this,props.tolink}>
+                <Link to ={this.props.tolink}>
                     {this.props.item}
                 </Link>
             </li>
