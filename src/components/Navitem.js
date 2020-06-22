cced8ce70d8c61fb6bbcdcc4d54890649dd1b594
Claudia Petty <claudialove32@gmail.com>
2020-06-22 14:28:15 -0400

    finish navbar and navitem with prop: activec which sends function/method to navitem when its called

--- src/components/Navitem.js
+++ src/components/Navitem.js
@@ -1,19 +1,16 @@
 //importing Link which acts like an anchor tag in react
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-
 class Navitem extends Component {
     render() {
         return (
-            <li>
-                <Link to ={this.props.tolink}>
-                    {this.props.item}
-                </Link>
+            <li id={this.props.item}>
+                <Link to={this.props.tolink} onClick={this.props.activec.bind(this, this.props.item)}>{this.props.item}</Link>
             </li>
         )
     }
 }
-
-
+ 
+//bind method above is used to to bind the current instance and the input to be sent
 export default Navitem;
 
