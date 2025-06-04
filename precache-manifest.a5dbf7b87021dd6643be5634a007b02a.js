4d8b14e23f119a13e0c05f8c9349d7c402b953cc
Claudia Barkett <claudialove32@gmail.com>
2025-06-04 16:59:12 -0400

    Updates

---
+++ precache-manifest.a5dbf7b87021dd6643be5634a007b02a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49474b9927a40c1234e396840eeb9798",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "4dafa7577ee8825c0863",
+    "url": "/react-portfolio/static/css/main.f742b8ff.chunk.css"
+  },
+  {
+    "revision": "b3286077846232ef26be",
+    "url": "/react-portfolio/static/js/2.b7383c9f.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/react-portfolio/static/js/2.b7383c9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dafa7577ee8825c0863",
+    "url": "/react-portfolio/static/js/main.fcda69fa.chunk.js"
+  },
+  {
+    "revision": "a8e24379f363a3212a68",
+    "url": "/react-portfolio/static/js/runtime-main.a72caf4b.js"
+  },
+  {
+    "revision": "f3a7e0c69e375e055ccb95d41a9c652c",
+    "url": "/react-portfolio/static/media/candies.f3a7e0c6.PNG"
+  },
+  {
+    "revision": "acfe0118c4f70fe4c5c5abc88c42d8fa",
+    "url": "/react-portfolio/static/media/cat.acfe0118.PNG"
+  },
+  {
+    "revision": "d64b82a51732712c6ce410214e9df7a0",
+    "url": "/react-portfolio/static/media/claude.d64b82a5.PNG"
+  },
+  {
+    "revision": "be239c52bf2a11cd519900b5d553d86d",
+    "url": "/react-portfolio/static/media/password.be239c52.PNG"
+  },
+  {
+    "revision": "83452428ad4d41fcbc6cd3bbfbb16a8e",
+    "url": "/react-portfolio/static/media/pattern.83452428.PNG"
+  },
+  {
+    "revision": "fa31bae21579919626064d99dc37e1af",
+    "url": "/react-portfolio/static/media/quiz.fa31bae2.PNG"
+  },
+  {
+    "revision": "1df244c4983ad0da0e930d4f42ae8964",
+    "url": "/react-portfolio/static/media/sky.1df244c4.PNG"
+  }
+]);
